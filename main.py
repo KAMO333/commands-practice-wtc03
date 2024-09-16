16e22c36acc92ebf1ee038120ff8e005a161ab20
Kamogelo Mmopane <morokammopane@gmail.com>
2024-09-16 15:54:44 +0200

    modified subtract and divide functions

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ def add(a, b):
     sum = a + b
     return sum
 print(add(1, 9))
+
 def multiply(a,b):
     product = a*b
     return product
 print(multiply(5,2))
 
-def subtract():
-    ...
+def subtract(x, y):
+    return x - y
 
-def divide():
-    ...
+print(subtract(23, 56))
+
+def divide(a, b):
+    return a / b
+
+print(divide(2, 4))
