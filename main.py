6d43c61531e2dc0be75bcac8d0d68f3d4c1aa55b
Lindiwe Mfene <limfenejhb024@student.wethinkcode.co.za>
2024-09-16 15:47:39 +0200

    modified multiplcation

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ def add(a, b):
     sum = a + b
     return sum
 print(add(1, 9))
-def multiply():
-    ...
+def multiply(a,b):
+    product = a*b
+    return product
+print(multiply(5,2))
 
 def subtract():
     ...
